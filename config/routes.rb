0e344a09940fdc8bbd09b956c4d2152556524964
hannan228 <abdulhannan1146@gmail.com>
2022-01-12 13:00:00 +0500

    root changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes pern the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  
   # resources :articles, only: [:show, :index, :new, :create, :update, :edit]
   resources :articles 
-  root "pages#home"
+  root "articles#index"
+  
 
 end
